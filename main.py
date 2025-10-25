4e333743049cc8c3d0b932f80eda14ab0d0deff5
Jake Leuthardt <jakeleuthardt1@gmail.com>
2025-10-24 19:57:36 -0500

    make menu

--- main.py
+++ main.py
@@ -30,7 +30,18 @@ while running:
     screen.fill((0, 0, 0))
 
     if not gameStarted:  # Menu
-        pass  # (Draw menu here if desired)
+        # Simple menu with title and instructions
+        font_title = pygame.font.SysFont(None, 64)
+        font_instr = pygame.font.SysFont(None, 36)
+        title_surf = font_title.render("FNAF Clone", True, (255,255,255)) # WORKING TITLE
+        instr_surf = font_instr.render("Press ENTER to Start", True, (255,255,255))
+
+        # Center title and instruction on screen
+        title_rect = title_surf.get_rect(center=(400, 200))
+        instr_rect = instr_surf.get_rect(center=(400, 300))
+
+        screen.blit(title_surf, title_rect)
+        screen.blit(instr_surf, instr_rect)
     else:  # Gameplay
         screen.blit(map, (0,0))
 
