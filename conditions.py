5158f081cee0da435f7d41f46f3b1db85cbf7384
jake l school acct <239911@shakopeeschools.org>
2025-10-28 11:17:03 -0500

    add a variable end time

--- conditions.py
+++ conditions.py
@@ -1,9 +1,9 @@
-from re import S
+import random
 import pygame
 
 class GameConditions:
     def __init__(self):
-        self.gameLength = 28800 # 8 mins, in frames
+        self.gameLength = random.randint(22400, 28800) # any frame between like 6m 13s and 8m, for variety
         self.currentFrame = 0 # gets updated once per frame
 
     def incrFrameCt(self):
